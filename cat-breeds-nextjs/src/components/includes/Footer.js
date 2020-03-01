faf82f368557d010316c48098f03607588f112af
videoeero-utu <eero.suvanto@gmail.com>
2020-03-01 22:27:49 +0200

    Style header and footer

--- cat-breeds-nextjs/src/components/includes/Footer.js
+++ cat-breeds-nextjs/src/components/includes/Footer.js
@@ -3,8 +3,8 @@ import React, { Component } from 'react';
 class Footer extends Component {
   render() {
     return (
-      <footer className='flex flex__column'>
-        <p>&copy; 2020 People's Front of Catto</p>
+      <footer className='footer'>
+        <p className='footer__text'>&copy; 2020 People's Front of Catto</p>
       </footer>
     );
   }
--- cat-breeds-nextjs/src/components/includes/Header.js
+++ cat-breeds-nextjs/src/components/includes/Header.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import NerdCat from '../../img/NerdCat';
+import NerdCatAlt from '../../img/NerdCatAlt';
 
 class Header extends Component {
   render() {
@@ -7,12 +8,12 @@ class Header extends Component {
       <>
         <header className='header'>
           <div className='header__img'>
-            <NerdCat />
+            <NerdCatAlt />
           </div>
           <div className='header__text'>
-            <h1 className='heading__h1'>Cat Breeds</h1>
+            <h1 className='heading__h1'>Cat Knowledge</h1>
             <p className='header__tagline'>
-              Knowledge is power, when cats start their world domination
+              You want to be prepared for their world domination
             </p>
           </div>
         </header>
