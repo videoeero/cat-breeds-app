06e888053b4344ff33f5776e68b3ddbaf093f93b
videoeero-utu <eero.suvanto@gmail.com>
2020-03-01 15:19:07 +0200

    Add db env var

--- cat-breeds-nextjs/next.config.js
+++ cat-breeds-nextjs/next.config.js
@@ -1,5 +1,8 @@
 const withSass = require('@zeit/next-sass');
 module.exports = withSass({
+  env: {
+    DB_URL: 'http://localhost:3000/api/v1/catBreeds'
+  },
   webpack(config, options) {
     config.module.rules.push({
       test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
