41dd554f7767410ef875bf76c7f67f2b523925bd
videoeero-utu <eero.suvanto@gmail.com>
2020-03-02 21:47:06 +0200

    Add origins array to initial props

--- cat-breeds-nextjs/pages/_app.js
+++ cat-breeds-nextjs/pages/_app.js
@@ -2,21 +2,23 @@ import App from 'next/app';
 import React from 'react';
 import MainLayout from '../src/components/layouts/mainLayout';
 import fetch from 'isomorphic-unfetch';
+import getOriginsArray from '../src/utils/getOriginsArray';
 import '../src/styles/main.scss';
 
 class CatBreeds extends App {
   static async getInitialProps(ctx) {
     const res = await fetch(process.env.DB_URL);
     const json = await res.json();
-    return { cats: json };
+    const origins = await getOriginsArray(json.data.catBreeds);
+    return { cats: json, origins: origins };
   }
 
   render() {
-    const { Component, pageProps, cats } = this.props;
+    const { Component, pageProps, cats, origins } = this.props;
 
     return (
       <MainLayout>
-        <Component {...pageProps} cats={cats} />
+        <Component {...pageProps} cats={cats} origins={origins} />
       </MainLayout>
     );
   }
