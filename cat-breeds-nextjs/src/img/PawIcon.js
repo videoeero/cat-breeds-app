988a10ca8f7c891c48722282a51a394f0e98ef14
videoeero-utu <eero.suvanto@gmail.com>
2020-03-03 22:42:46 +0200

    Clean up

--- cat-breeds-nextjs/src/img/PawIcon.js
+++ cat-breeds-nextjs/src/img/PawIcon.js
@@ -3,7 +3,6 @@ import React from 'react';
 export default function PawIcon({ index }) {
   return (
     <svg
-      // className='icon__paw fadeInPaw'
       className={index == 0 ? 'icon__paw fadeInNormal' : 'icon__paw fadeInPaw'}
       xmlns='http://www.w3.org/2000/svg'
       viewBox='0 0 61.4 57.6'
